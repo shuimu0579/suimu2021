fbe22c7341c7731b974068cc3397476825f0cccd
shuimu0579 <shuimu0579@gmail.com>
2021-07-01 14:21:06 +0800

    change

--- test01.js
+++ test01.js
@@ -1,37 +1,9 @@
-// [Tasks, microtasks, queues and schedules](https://jakearchibald.com/2015/tasks-microtasks-queues-and-schedules/)
-
-async function foo() {
-  console.log('foo')
-}
-
-async function bar() {
-  console.log('bar start')
-  await foo()
-  console.log('bar end')
-}
-
-console.log('script start')
-
-setTimeout(function() {
-  console.log('setTimeout')
-}, 0)
-
-bar()
-
-new Promise(function(resolve) {
-  console.log('promise executor')
-  resolve()
-}).then(function() {
-  console.log('promise then')
-})
-
-console.log('script end')
-
-// script start
-// bar start
-// foo
-// promise executor
-// script end
-// bar end
-// promise then
-// setTimeout
+var name = 'World!'
+;(function() {
+  if (typeof name === 'undefined') {
+    var name = 'Jack'
+    console.log('Goodbye ' + name)
+  } else {
+    console.log('Hello ' + name)
+  }
+})()
