256b43a4cad53b808d08260f6ef6d5f4f9597ce4
shuimu0579 <shuimu0579@gmail.com>
2021-07-03 15:37:51 +0800

    change

--- test01.js
+++ test01.js
@@ -1,5 +1,14 @@
 var name = 'World!'
 ;(function() {
+  // 由以下两行代码可知：name === undefined
+  console.log('name', name)
+  console.log('undefined', undefined)
+  // 由以下两行代码可知：typeof name === "undefined"
+  console.log('typeof undefined', typeof undefined)
+  console.log('typeof name', typeof name)
+
+  console.log(name === undefined)
+  console.log(name === 'undefined')
   if (typeof name === 'undefined') {
     var name = 'Jack'
     console.log('Goodbye ' + name)
